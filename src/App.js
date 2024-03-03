06d3fd1ad0f19a81e22c61538ebc423b7f6ddf36
saatvik-10 <saatvikmadan529@gmail.com>
2024-03-03 23:59:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './pages/Home/Home';
 import Kmain from './pages/Buildings/Kashyap/Kmain';
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import DrawerAppBar from './Header';
 import Footer from './Footer';
+import midPart from './midPart';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <DrawerAppBar />
+      <midPart/>
       <div className='content'>{children}</div>
       <Footer />
     </div>
