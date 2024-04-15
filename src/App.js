3cb2fee6bc78e803873ee1391a20a7b378fd153f
Sanika Mulik <sanikamulik24@gmail.com>
2024-04-15 20:16:41 +0530

    staff commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './pages/Home/Home';
 // import Kmain from './pages/Buildings/Kashyap/Kmain';
-// import KS102 from './pages/Buildings/Kashyap/staffroom/ks102';
+import KS102 from './pages/Buildings/Kashyap/staffroom/ks102';
 import { Routes, Route } from 'react-router-dom';
 import CampusMap from './pages/CampusMap/Cmap';
 import Class from './components/classroom/Kashyap/Class';
@@ -27,6 +27,9 @@ function App() {
         <Route path='/ks/floor-1' element={<Floor1 />} />
         <Route path='/ks/floor-0' element={<Floor0 />} />
         <Route path='/campus-map' element={<CampusMap />} />
+        <Route path='/ks102' element={<KS102 />} />
+
+        
       </Routes>
     </div>
   );
